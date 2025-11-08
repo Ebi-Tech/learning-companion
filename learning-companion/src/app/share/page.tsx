d3b97ea7f2ff2915e336289388ede203e65ee2eb
Ebi-Tech <difebi14@gmail.com>
2025-11-08 12:50:45 +0200

    Wrapped my useSearchParams() in suspense boundary, tryng to fix vercel deploy error

--- learning-companion/src/app/share/page.tsx
+++ learning-companion/src/app/share/page.tsx
@@ -1,6 +1,11 @@
 // src/app/share/page.tsx
+import { Suspense } from 'react';
 import ShareView from '@/components/ShareView';
 
 export default function SharePage() {
-  return <ShareView />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ShareView />
+    </Suspense>
+  );
 }

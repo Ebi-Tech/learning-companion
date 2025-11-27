e61ce5e58cb6cd963ffcea21383bccdd9b854abd
Ebi-Tech <difebi14@gmail.com>
2025-11-27 03:32:03 +0200

    deleted vercel.json

--- learning-companion/next.config.ts
+++ learning-companion/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+ generateBuildId: async () => {
+    return process.env.VERCEL_GIT_COMMIT_SHA || `build-${Date.now()}`
+  },
+  // chunk handling
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxInitialRequests: 25,
+        minSize: 20000,
+      }
+    }
+    return config
+  }
+}
 
 export default nextConfig;
